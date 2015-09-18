d62793bed03043597a89f57a1be7615a655429b4
Timothy Lee <tim@codepath.com>
2015-09-18 15:03:21 -0700

    Initial Commit

---
+++ Hamburger Menu DemoTests/Hamburger_Menu_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hamburger_Menu_DemoTests.swift
+//  Hamburger Menu DemoTests
+//
+//  Created by Timothy Lee on 9/18/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import XCTest
+@testable import Hamburger_Menu_Demo
+
+class Hamburger_Menu_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
